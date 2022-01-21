fbdb5c9338a3f52893b76cbff939884496ae4092
Minyillah George Mensah <george.minyillah@ashesi.edu.gh>
2022-01-21 18:22:04 +0000

    Add files via upload

---
+++ George_Minyillah/hw0_cpp/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, I am all set up for c++ for ICP" << endl;
+    return 0;
+}
---
+++ George_Minyillah/hw0_java/homework.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package junior_second_semester;
+
+/**
+ * @author minyillah
+ *
+ */
+public class homework {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, I am all set up for Java for ICP");
+
+	}v
+
+}
