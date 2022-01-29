d778299294bd893e4af09fbb0d1954ff7675cce5
Kenneth Azortibah <kazortibah5562@gmail.com>
2022-01-29 21:55:49 +0000

    Phase 1 - #1, #2, #3

--- Kenneth-Azortibah/README.md
+++ Kenneth-Azortibah/README.md
@@ -6,3 +6,5 @@ Welcome to my corner of the internet!
 ## Journal
 - *29/01/2022*
 All set up and ready to go.
+The first phase of the project is hard- but no one said it was gonna be easy.
+Used HashMaps to create a working log-in account system! Now, everything else...
--- Kenneth-Azortibah/project_phase_1/Canvas/src/com/moony/Main.java
+++ Kenneth-Azortibah/project_phase_1/Canvas/src/com/moony/Main.java
@@ -1,8 +1,54 @@
 package com.moony;
 
+import java.util.Scanner;
+import java.util.HashMap;
+
 public class Main {
+    // global HashMap
+    public static HashMap<String, String> userAccounts = new HashMap <String, String>();
 
     public static void main(String[] args) {
         System.out.println("Let's get this over with.");
+
+        createAccount();
+        System.out.println(userAccounts);
+
+        login();
+    }
+
+    public static void createAccount() {
+        // saves user's email and password
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your desired email address below:");
+        String email = input.nextLine();
+        System.out.println("Set a secure password:");
+        String password = input.nextLine();
+
+        userAccounts.put(email, password);
+        System.out.println("Your account has been created.");
+    }
+
+    public static boolean login() {
+        // returns True if the user exists
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your email address:");
+        String email = input.nextLine();
+        System.out.println("Enter your password:");
+        String password = input.nextLine();
+
+        if (userAccounts.containsKey(email) && userAccounts.get(email).equals(password)) {
+            System.out.println("Logged in successfully. Welcome, " + email);
+            return true;
+        }
+        else {
+            System.out.println("Your account doesn't exist.");
+            return false;
+        }
+    }
+
+    public static void updateProfile() {
+
     }
 }
