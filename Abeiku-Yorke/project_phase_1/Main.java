5cb989a4ce4f052256441cca10dfbb5f834b8b89
Abeikubei <abeiku.yorke@ashesi.edu.gh>
2022-02-03 13:40:07 -0700

    Latest update

--- Abeiku-Yorke/project_phase_1/Main.java
+++ Abeiku-Yorke/project_phase_1/Main.java
@@ -1,53 +1,108 @@
 //Main Class
 public class Main {
+//Create an array of type user to keep track of all users
   private static User[] users = new User[1000];
+//Create an array of type Admin to keep track of all admins
   private static Admin[] admins = new Admin[1000];
+//Create an array of type Course
+  private static Course[] courses = new Course[1000];
+//This is to keep count of users
   private static int userCount = 0;
+//This is to keep count of admins
   private static int adminCount = 0;
+//This is to keep count of courses
+private static int courseCount = 0;
+//This is to check whether a user or admin is logged in, it has been set default to false
   private static boolean loggedIn = false;
+// when the user logins, currentUserEmail is set to the person's email.
   private static String currentUserEmail = "";
   
-  
+
+
+//This is the createAccount function. It is made void because, it doesnt return anything.
   public static void createAccount(String email, String pass) {
+//Create a user instance, and since it was specified that we want only email and password, the rest of the parameters for the user constructior is made null
     User user = new User(email, pass, null, null, null, null, null);
+//This is where you add the user to the users array @ index"Usercount"  
     users[userCount] = user;
+//When a user is added the "UserCount" is incremented.
     userCount++;
   }
-  
+
+
+//This is the createAccount function. It is made void because, it doesnt return anything.
+public static void createAdminAccount(String email, String pass) {
+    boolean userExists = false;
+    for(int i = 0; i < userCount; i++){
+        if(users[i].email == email){
+            userExists = true;
+        }
+    }
+
+    if(userExists == false){
+    //Create a user instance, and since it was specified that we want only email and password, the rest of the parameters for the user constructior is made null
+        User user = new User(email, pass, null, null, null, null, null);
+    //This is where you add the user to the users array @ index"Usercount"  
+        users[userCount] = user;
+    //When a user is added the "UserCount" is incremented.
+        userCount++;
+    }
+        
+        Admin admin = new Admin(email,pass);
+        admins[adminCount] = admin;
+        adminCount++;
+      }
+    
+//This is to check whether a User exists. As such it should return a boolean value.
   public static boolean login(String email, String pass) {
+//This for loop is used to go through the user array.
     for(int i = 0; i<userCount; i++){
+// Checking if email and password are the same 
       if(users[i].email == email && users[i].password == pass){
+//If they are you set it to true
         loggedIn = true;
+//You keep track of the user email
         currentUserEmail = email;
+//Return true
         return true;
       }
     }
     return false;
   }
-  
+//Create the UpdateProfile. it is not returning anything so it is void. 
   public static void updateProfile(String email, String fname, String lname, String yearGroup, String currentGpa, String dob) {
+//If the user is logged, in
     if(loggedIn){
+//A for loop is created to run through the user array
       for(int i = 0; i<userCount; i++){
+//if the email matches and it also matches the currentUser, then allow the person to update his/her info
         if(users[i].email == email && email == currentUserEmail){
           users[i].fname = fname;
           users[i].lname = fname;
           users[i].yearGroup = yearGroup;
           users[i].currentGpa = currentGpa;
           users[i].dob = dob;
-          
+  
           System.out.println("Profile Updated Successfully");
         }
       }
     }else{
-      System.out.println("Error: Kindly check your email or password");
+      System.out.println("Error: Kindly login first");
     }
     
   }
-  
+
+
+ 
   public static void viewProfile(String email) {
+
+    //If logged in
     if(loggedIn){
+    // loop through user array
       for(int i = 0; i<userCount; i++){
+        //if the email is the same
         if(users[i].email == email && email == currentUserEmail){
+        //If the users fname is not equal to null, print the user name else, print NA
           System.out.println(users[i].fname != null ? users[i].fname : "NA");
           System.out.println(users[i].lname != null ? users[i].lname : "NA");
           System.out.println(users[i].yearGroup != null ? users[i].yearGroup : "NA");
@@ -56,7 +111,7 @@ public class Main {
         }
       }
     }else{
-      System.out.println("Error: Kindly check your email or password");
+      System.out.println("Error: Kindly login");
     }
   }
   
@@ -77,7 +132,7 @@ public class Main {
         if(admins[i].email == currentUserEmail){
 //           loop through users array
           for(int j = 0; j<userCount; j++){
-//             if uer is found
+//             if user is found
             if(users[j].email == email){
 //               make user faculty
               users[j].isFaculty = true;
@@ -98,24 +153,47 @@ public class Main {
     }
     return false;
   }
+
+
+  public static void createCourse(String email,String coursename,String coursecode){
+       if(loggedIn){
+           if(isFaculty(email)){
+               Course course = new Course(email,coursename,coursecode);
+               courses[courseCount] = course;
+               courseCount++;
+               System.out.println("The course has been created successfully");
+           }
+           else{
+               System.out.println("You are not a Faculty");
+           }
+        } else{
+            System.out.println("Error:Kindly login");
+        }
+    }
+
+
+
+ public static void viewCourses(){
+     for (int i = 0; i < courseCount; i++){
+         for(int j = 0; j < userCount; j++){
+             if(courses[i].email == users[j].email){
+                 System.out.println("Course Code: " +courses[i].coursecode + " || Course Name: " + courses[i].coursename + " || Email: " + courses[i].email + " || Instructor Name: " + users[j].fname);
+             }
+         }
+
+     }
+
+ }
   
   
   public static void main(String[] args) {
-    createAccount("abeiku.yorke@ashesi.edu.gh", "abeiku");
-    System.out.println(login("abeiku.yorke@ashesi.edu.gh", "abeiku"));
-    updateProfile("abeiku.yorke@ashesi.edu.gh", "Abeiku", "Yorke", "2022", "3.0", "05/08/2000");
-    viewProfile("abeiku.yorke@ashesi.edu.gh");
-    System.out.println(isAdmin("abeiku.yorke@ashesi.edu.gh"));
-    
-//  create an admin and insert into admins array at index 0
-    admins[0] = new Admin("abeiku.yorke@ashesi.edu.gh");
-//    increement adminCount
-    adminCount++;
-    
-    System.out.println(isAdmin("abeiku.yorke@ashesi.edu.gh"));
-    makeFaculty("abeiku.yorke@ashesi.edu.gh");
-    System.out.println(isFaculty("abeiku.yorke@ashesi.edu.gh"));
-     
+    createAccount("abeiku.yorke@ashesi.edu.gh","123");
+    System.out.println(login("abeiku.yorke@ashesi.edu.gh","123"));
+    createAdminAccount("abeiku.yorke@ashesi.edu.gh","123");
+    createAccount("opanaplange@ashesi.edu.gh","123");
+    makeFaculty("opanaplange@ashesi.edu.gh");
+    createCourse("opanaplange@ashesi.edu.gh","ICP","ICP213");
+    viewCourses();
   }
  
 }
@@ -150,9 +228,27 @@ class User{
 //class for Admin
 class Admin{
   public String email;
+  public String password;
   
   // Admin class constructor
-  Admin(String email){
+  Admin(String email,String password){
     this.email = email;
+    this.password = password;
   }
+}
+
+//class for Course
+class Course{
+    public String email;
+    public String coursename;
+    public String coursecode;
+    
+
+// Course class constructor
+Course(String email,String coursename,String coursecode){
+    this.email = email;
+    this.coursename = coursename;
+    this.coursecode = coursecode;
+}
+
 }
