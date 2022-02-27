3505904868da3f8ae72e78619a72faf40fc2590f
Yomynameszekae <79933418+Yomynameszekae@users.noreply.github.com>
2022-02-27 22:47:15 +0000

    Create CanvasFunctions.java

---
+++ Kelvin_Zangu/project_phase_2/CanvasFunctions.java
@@ -0,0 +1,31 @@
+
+public interface CanvasFunctions {
+	void createAccount();
+	void updateProfile();
+	void viewProfile();
+	boolean isAdmin();
+	void makeFaculty();
+	boolean isFaculty();
+	void createCourse();
+	void takeEmail();
+	void takePassword();
+	void takeAssName();
+	void takeCourseCode();
+	void viewCourses();
+	void registerCourse();
+	void viewCourseByCode();
+	void viewCourseByEmail();
+	void addAssignment();
+	void viewAssignmentsByCourse();
+	void viewAssignmentsByEmail();
+	void submitAssignment();
+	void viewSubmissions();
+	void scoreAssignment();
+	void viewAssignmentScore();
+	void viewAssignmentScores();
+	void viewAllAssignmentScore();
+	void viewAssignmentGrade();
+	void viewAssignmentGrades();
+	void viewAllAssignmentGrades();
+	void setNotificationPreferences();	
+}
